31379ce52e9eb0ea6b5bc2a50d4281ef582f5137
kristen_mendiola <kmendiola.work@gmail.com>
2024-07-06 20:33:10 -0400

    revised functions

--- crewsite/functions.py
+++ crewsite/functions.py
@@ -2,23 +2,6 @@
 # from twscrape import API, gather
 # from twscrape.logger import set_log_level
 
-# async def scrape():
-#     api = API()
-#     await api.pool.add_account("kit_kitapi", "13manaboy", "kit.tweetapi@gmail.com", "Kenzoku<3")
-#     await api.pool.login_all()
-#     user_id = 840052593690890240
-#     scraper_list = await gather(api.user_tweets(user_id, limit=1, kv={"product": "Latest"}))
-#     return scraper_list[:5]
-
-# import tweepy
-
-# # Replace these with your own credentials
-# consumer_key = 'oMCXIHE86OlxpSgVF5VNDUkWz'
-# consumer_secret = 'iWyIUIIX7uwK3ntuVwgECHDYkCff7Aafh0LOfPb1ZMRd5kQcHw'
-# access_token = '783157230342246400-zrjAbjPAawOPtBeXyxlmfFfAKPkJ8ip'
-# access_token_secret = '2c2MHL7WlaGDoKVQvzZ1NeR4Pho2TsTVFTnwkwgnGrkYY'
-# bearer_token = 'AAAAAAAAAAAAAAAAAAAAALtnuAEAAAAAnRYY6zVDjQFQk4BNjGdH1cpX4O8%3DGaeawdPmj2i1qbuFgYGgdIF1jKIRz8Oz0XrhoQVbjYOOpGYDRv'
-
 # # Authenticate to Twitter
 # client = tweepy.Client(bearer_token=bearer_token)
 
